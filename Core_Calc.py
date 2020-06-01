698840fb8a0044873a1c9d29137342330c8add10
BlueVanderbilt <46427478+BlueVanderbilt@users.noreply.github.com>
2020-06-01 10:00:02 -0400

    Update Core_Calc.py

--- Core_Calc.py
+++ Core_Calc.py
@@ -1,22 +1,28 @@
+import datetime, math
+
+now = datetime.datetime.now()
+if (now.year%4==0 and now.year%100!=0 or now.year%400==0): leap = "true"
+else: leap = "false"
+    
 def core_calc(month, event, event_days, core):
-    if month == 2: total = 28
+    if month == 2 and leap == "true": total = 29
+    elif month == 2 and leap == "false": total = 28
     elif month in (4, 6, 9, 11): total = 30
     elif month in (1, 3, 5, 7, 8, 10, 12): total = 31
     else: print("Invalid month.")
     if event == 1:
-        left = total - event_days; week_core = round(total/7); print((core * 3 * left) + (core * 3 * event_days * 3) + total * 5 + week_core * 15)
-    else: week_core = round(total/7); print(core * 3 * total + total * 5 + week_core * 15)
+        left = total - event_days; week_core = math.floor(total/7); print((core * 3 * left) + (core * 3 * event_days * 3) + total * 5 + week_core * 15)
+    else: week_core = math.floor(total/7); print(core * 3 * total + total * 5 + week_core * 15)
 
 '''
 Instructions on how to use:
 
 1st argument is Month, any number ranging from 1 to 12
-2nd argument is event, 1 means yes and any other number means no. 
-3rd argument is event_days, pretty self explainable
-4th argument is core, just means how many core data do you get per clear, F.E: Chapter 8 would be 20 core data per clear
+2nd argument is event, 1 means yes and any other number means no
+3rd argument is event_days, how many days the 3x core event lasts for
+4th argument is core, just means how many core data do you get per clear, e.g: Chapter 8 would be 20 core data per clear
 
-Lastly, this code is 100% accurate for non leap years as 28/7 = 4, 30/7 = 4.28 (Rounded to 4), 31/7 = 4.42 (Rounded to 4)
-If calculating core data for leap year and the month of February, just +5 core data to the output.
+Lastly, this code is 100% accurate. 
 
 '''
 
