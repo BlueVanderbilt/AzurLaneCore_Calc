9ef523c5ba0c09c6375f46443cfca3b7c4ec3f18
BlueVanderbilt <46427478+BlueVanderbilt@users.noreply.github.com>
2020-06-02 17:31:58 -0400

    Update Core_Calc.py

--- Core_Calc.py
+++ Core_Calc.py
@@ -1,4 +1,4 @@
-import datetime, math
+import datetime
 
 x = datetime.datetime.now()
 if x.year % 4 == 0 and (x.year % 100 != 0 or x.year % 400 == 0): leap = "true"
@@ -11,8 +11,8 @@ def core_calc(month, event, event_days, core):
     elif month in (1, 3, 5, 7, 8, 10, 12): total = 31
     else: print("Invalid month.")
     if event == 1:
-        left = total - event_days; week_core = math.floor(total/7); print((core * 3 * left) + (core * 3 * event_days * 3) + total * 5 + week_core * 15)
-    else: week_core = math.floor(total/7); print(core * 3 * total + total * 5 + week_core * 15)
+        left = total - event_days; week_core = round(total/7); print((core * 3 * left) + (core * 3 * event_days * 3) + total * 5 + week_core * 15)
+    else: week_core = round(total/7); print(core * 3 * total + total * 5 + week_core * 15)
 
 '''
 Instructions on how to use:
