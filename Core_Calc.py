d7bb8b44aaa11ebe467247471b7a9061e188085b
BlueVanderbilt <46427478+BlueVanderbilt@users.noreply.github.com>
2020-06-13 01:23:12 -0400

    Update Core_Calc.py
    Code has been shortened, below is a an explanation of what I have done. 
    
    math.floor(28/7), math.floor(29/7), math.floor(30/7) and math.floor(31/7) all give an output of 4. This number 4 refers to how many weeks any month can have. The total amount of core from the weekly task is therefore 4 * 15 = 60. Thus there is no need for a week_core variable. 
    
    Condensed a 4 line code or so with a ternary operator.

--- Core_Calc.py
+++ Core_Calc.py
@@ -3,26 +3,22 @@ import datetime
 x = datetime.datetime.now()
 leap = 1 if x.year % 4 == 0 and (x.year % 100 != 0 or x.year % 400 == 0) else 0
 
-def core_calc(month, event, event_days, core):
+def core_calc(month, event, days, core):
     if month == 2 and leap == 1: total = 29
     elif month == 2 and leap == 0: total = 28
     elif month in (4, 6, 9, 11): total = 30
     elif month in (1, 3, 5, 7, 8, 10, 12): total = 31
-    if event == 1:
-        try: left = total - event_days; week_core = round(total/7); print((core * 3 * left) + (core * 3 * event_days * 3) + total * 5 + week_core * 15)
-        except: print("Invalid Month")
-    elif event == 0: 
-        try: week_core = round(total/7); print(core * 3 * total + total * 5 + week_core * 15)
-        except: print("Invalid Month")
-    else: print("Invalid input for event: 1 = Yes, 0 = No")
+    try: left = total - days; print((core * 3 * left) + (core * 3 * days * 3) + total * 5 + 60) if event == 1 else print(core * 3 * total + total * 5 + 60)
+    except: print("Invalid Month")
         
 '''
-Instructions on how to use:
-1st argument is Month, any number ranging from 1 to 12
-2nd argument is event, 1 means yes and 0 means no 
-3rd argument is event_days, how many days the 3x core event lasts for
-4th argument is core, means how many core data do you get per clear, e.g: Chapter 8 would be 20 core data per clear
-Lastly, this code is 100% accurate. 
+Instructions on how to use)
+
+1st argument is Month: any number ranging from 1 to 12
+2nd argument is event: 1 means yes and any other number means no 
+3rd argument is days: refers to how many days the 3x core event lasts for
+4th argument is core: how many core data do you get per clear, e.g: Chapter 8 would be 20 core data per clear
+Lastly, the output is 100% accurate. 
 '''
 
 core_calc(6, 1, 9, 20) #This here is a template, change these numbers to calculate what you want. 
