32e7e5a75df4dcddd72698ff0e3e53bc6498965a
BlueVanderbilt <46427478+BlueVanderbilt@users.noreply.github.com>
2020-06-20 00:30:53 -0400

    Update Core_Calc.py
    Ternary operator

--- Core_Calc.py
+++ Core_Calc.py
@@ -4,9 +4,8 @@ x = datetime.datetime.now()
 leap = 1 if x.year % 4 == 0 and (x.year % 100 != 0 or x.year % 400 == 0) else 0
 
 def core_calc(month, event, days, core):
-    if month == 2 and leap == 1: total = 29
-    elif month == 2 and leap == 0: total = 28
-    elif month in (4, 6, 9, 11): total = 30
+    total = 29 if month == 2 and leap == 1 else 28 
+    if month in (4, 6, 9, 11): total = 30
     elif month in (1, 3, 5, 7, 8, 10, 12): total = 31
     try: left = total - days; print((core * 3 * left) + (core * 9 * days) + total * 5 + 60) if event == 1 else print(core * 3 * total + total * 5 + 60)
     except: print("Invalid Month")
@@ -14,10 +13,10 @@ def core_calc(month, event, days, core):
 '''
 Instructions on how to use)
 
-1st argument is month: any number ranging from 1 to 12
+1st argument is month: any number ranging from 1 through 12
 2nd argument is event: 1 means yes and any other number means no 
 3rd argument is days: refers to how many days the 3x core event lasts for
-4th argument is core: how many core data do you get per clear, e.g: Chapter 8 would be 20 core data per clear
+4th argument is core: how much core data you get per clear, e.g: Chapter 8 would be 20 core data per clear
 Lastly, the output is 100% accurate. 
 '''
 
